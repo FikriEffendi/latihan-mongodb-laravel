8967e131b66001a3ffc224a03356230f468788d6
aerochops <150325960+aerochops@users.noreply.github.com>
2024-11-17 00:27:35 +0700

    membuat view index

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+
+<body>
+    <div class="border-2"
+        <div class="">
+        <div class="container flex justify-between divide-x-2">
+            <div>
+                Nomor
+            </div>
+            <div class="">
+                Judul
+            </div>
+            <div>
+                deskripsi
+            </div>
+            <div>
+                action
+            </div>
+        </div>
+        <div class="container">
+            <div>halo</div>
+            <div>halo1</div>
+        </div>
+    </div>
+    </div>
+</body>
+
+</html>
