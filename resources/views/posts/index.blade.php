11ca3dbf98b27c0e093e7cd0c0b92ba5e6c5adec
aerochops <150325960+aerochops@users.noreply.github.com>
2024-11-18 22:57:53 +0700

    memperbaiki table

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,31 +6,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-
 </head>
 
-<body>
-    <div class="border-2"
-        <div class="">
-        <div class="container flex justify-between divide-x-2">
-            <div>
-                Nomor
-            </div>
-            <div class="">
-                Judul
-            </div>
-            <div>
-                deskripsi
-            </div>
-            <div>
-                action
-            </div>
-        </div>
-        <div class="container">
-            <div>halo</div>
-            <div>halo1</div>
-        </div>
-    </div>
+<body class="p-8">
+    <div>
+        <table class="w-full border border-gray-300">
+            <!-- Bagian Header -->
+            <thead class="bg-gray-200">
+                <tr>
+                    <th class="px-4 py-2 text-center border">Nomor</th>
+                    <th class="px-4 py-2 text-center border">Judul</th>
+                    <th class="px-4 py-2 text-center border">Isi</th>
+                    <th class="px-4 py-2 text-center border">Action</th>
+                </tr>
+            </thead>
+            <!-- Bagian Body -->
+            <tbody class="divide-y divide-gray-300">
+                <tr>
+                    <td class="px-4 py-4 text-center">1</td>
+                    <td class="px-4 py-4">Testing</td>
+                    <td class="px-4 py-4">Ini adalah testing</td>
+                    <td class="px-4 py-4">
+                        <div class="flex items-center justify-center gap-4">
+                            <button class="text-red-500 hover:underline">Hapus</button>
+                            <button class="text-blue-500 hover:underline">Delete</button>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="px-4 py-4 text-center">2</td>
+                    <td class="px-4 py-4">Halo</td>
+                    <td class="px-4 py-4">Ini adalah Halo</td>
+                    <td class="px-4 py-4">
+                        <div class="flex items-center justify-center gap-4">
+                            <button class="text-red-500 hover:underline">Hapus</button>
+                            <button class="text-blue-500 hover:underline">Delete</button>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </body>
 
